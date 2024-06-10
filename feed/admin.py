0fbc4584fb2632145f47dcbad9e38eede36d9fd2
Oleksandr <alexander_vasilenko@outlook.com>
2024-06-10 16:06:46 +0300

    Created PhotoInLine class; changed PostAdmin class in admin.py. So Photo can be uploaded together with creating post on admin panel

--- feed/admin.py
+++ feed/admin.py
@@ -3,11 +3,19 @@ from django.contrib import admin
 from feed.models import Post, Comment, Photo, Like
 
 
+class PhotoInline(admin.StackedInline):
+    model = Photo
+
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ('user', "created", "id")
     list_filter = ('user', "created")
     search_fields = ('user__username',)
     date_hierarchy = 'created'
+    ordering = ('-created',)
+    inlines = [
+        PhotoInline
+    ]
 
 
 class CommentAdmin(admin.ModelAdmin):
@@ -20,5 +28,4 @@ class CommentAdmin(admin.ModelAdmin):
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comment, CommentAdmin)
-admin.site.register(Photo)
 admin.site.register(Like)
