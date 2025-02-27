8d8e0e9e703d6cd4553fa3a7df59e958952d45a5
Oleksandr <alexander_vasilenko@outlook.com>
2025-02-27 14:25:20 +0200

    fix: paste collectstatic command after build webpack

--- start.sh
+++ start.sh
@@ -6,10 +6,10 @@ pip install -r requirements.txt
 echo "Applying database migrations..."
 python manage.py migrate --noinput
 
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
 echo "Building frontend assets with Webpack..."
 npm install -D webpack-cli
 npm run build
 npx webpack --mode production
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
