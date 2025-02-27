b87816d125f4f1dadf509d988134563f3b3ed0b9
Oleksandr <alexander_vasilenko@outlook.com>
2025-02-27 14:12:49 +0200

    fix: deleted initialising virtual environment in start.sh, so gunicorn can run outside venc

--- start.sh
+++ start.sh
@@ -1,23 +1,6 @@
 echo "Starting DjangoGramm Deployment"
 
-if [ ! -d "venv" ]; then
-  echo "Virtual environment 'venv' not found. Creating one..."
-  python -m venv venv
-fi
-
-if [ -f "venv/bin/activate" ]; then
-  source venv/bin/activate
-elif [ -f "venv/Scripts/activate" ]; then
-  source venv/Scripts/activate
-else
-  echo "Error: Virtual environment activation script not found."
-  exit 1
-fi
-
-echo "Virtual environment activated"
-
 echo "Installing dependencies..."
-pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Applying database migrations..."
